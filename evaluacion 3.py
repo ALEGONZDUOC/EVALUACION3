cdd7698702f0308e5095dcbb9b2997dc80bdedea
Cristianup7 <Cri.urzua@duocuc.cl>
2024-06-28 19:56:56 -0400

    evaluación 3.py

--- evaluacion 3.py
+++ evaluacion 3.py
@@ -1,1 +1,15 @@
 import csv
+import random
+import re
+
+vehiculos = []
+
+def valpatente(patente):
+    # Patente válida debe tener el formato xxxx-00 o xx-0000
+    return bool(re.match(r'^[A-Za-z]{4}\d{2}$', patente) or re.match(r'^[A-Za-z]{2}\d{4}$', patente))
+
+def valmarca(marca):
+    return 2 <= len(marca) <= 10
+
+def valprecio(precio):
+    return precio >= 5000000
